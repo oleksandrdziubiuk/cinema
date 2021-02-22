13f17c5cdd1263ccb96134668a7f27042198e15a
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-02-22 12:28:30 +0200

    Fixed hash password

--- src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -6,7 +6,6 @@ import com.dev.cinema.service.RoleService;
 import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import java.util.List;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,22 +13,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService cartService;
     private final RoleService roleService;
-    private final PasswordEncoder passwordEncoder;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService cartService, RoleService roleService,
-                                     PasswordEncoder passwordEncoder) {
+                                     ShoppingCartService cartService, RoleService roleService) {
         this.userService = userService;
         this.cartService = cartService;
         this.roleService = roleService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User register(String email, String password) {
         User user = new User();
         user.setEmail(email);
-        user.setPassword(passwordEncoder.encode(password));
+        user.setPassword(password);
         Role role = roleService.getRoleByName("User");
         user.setRoles(List.of(role));
         userService.add(user);
--- src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
@@ -4,18 +4,22 @@ import com.dev.cinema.dao.UserDao;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
 import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User add(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userDao.add(user);
     }
 
